2b91a1cb3730e60637eb0e7e840ec6234ea5e7d1
Zehnab <zyafzal@dundee.ac.uk>
2013-05-27 14:48:18 +0100

    Test basis before devise is implemented

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogTest::Application.config.secret_token = '4f208e1dca58afe2040c1365f1b5d4e92b3c1a8991d03b9150429b9c69fcf9cc0e199bd071c51ac8a9aa0e551c249ff6de6f3792a45c072f22712eaf653a2a8b'
